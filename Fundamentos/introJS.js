341e7f2ebb15d16741ee99d2bbc0a92241777c7d
JP <jpcp0614@gmail.com>
2023-07-19 18:18:44 -0300

    Variaveis

---
+++ Fundamentos/introJS.js
@@ -0,0 +1,15 @@
+var options = 10;
+console.log(options);
+
+var options = 15;
+console.log(options);
+
+const a = 12;
+
+let b = 10;
+
+function add (a, b) {
+  return console.log(a + b);
+}
+
+add(a, b);
