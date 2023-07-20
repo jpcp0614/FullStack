1885070bf5c5ee4f5cced73b582c6b17f295d0ba
valdenio458 <valdeniomartinho@gmail.com>
2023-07-20 15:49:23 -0300

    Template Literals

---
+++ Fundamentos/Arrays/templateLiterals.js
@@ -0,0 +1,9 @@
+const time = 'Flamengo';
+console.log(`O ${time} continua sendo o maior do mundo.`);
+
+let a = 12;
+let b = 25;
+console.log(`A soma entre ${a} e ${b} é ${a + b}.`);
+
+let note = 8;
+console.log(`${note >= 7 ? 'Approved!' : 'Disapproved!'}`)
