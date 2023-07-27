9886deb97d79b1ffd781e060be502ec82fc7b00a
jpcp0614 <jpcp0614@gmail.com>
2023-07-27 10:42:23 -0300

    :sparkles: feat - challenge 5

--- Fundamentos/Projetos/Playground_Functions/src/challenges.js
+++ Fundamentos/Projetos/Playground_Functions/src/challenges.js
@@ -22,9 +22,9 @@ function concatName(array) {
   return `${array[array.length - 1]}, ${array[0]}`;
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+//* Desafio 5
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
 // Desafio 6
