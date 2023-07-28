6995d3821a7f30c40c764233ee93d03cb521440f
jpcp0614 <jpcp0614@gmail.com>
2023-07-28 16:50:19 -0300

    Funções get

---
+++ Fundamentos/DOM/script.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("ID").innerHTML);
+
+console.log(document.getElementsByClassName("classP").item(0).innerText);
+
+console.log(document.getElementsByTagName("span").item(0));
