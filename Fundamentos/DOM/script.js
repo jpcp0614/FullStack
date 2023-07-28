deb33c2203526c6190810ec1f6fb3d5cb590c4dd
jpcp0614 <jpcp0614@gmail.com>
2023-07-28 16:59:28 -0300

    Funções query

--- Fundamentos/DOM/script.js
+++ Fundamentos/DOM/script.js
@@ -2,4 +2,8 @@ console.log(document.getElementById("ID").innerHTML);
 
 console.log(document.getElementsByClassName("classP").item(0).innerText);
 
-console.log(document.getElementsByTagName("span").item(0));
+console.log(document.getElementsByTagName("span").item(0).innerText);
+
+console.log(document.querySelector("p").innerHTML);
+
+console.log(document.querySelectorAll("p").item(2));
