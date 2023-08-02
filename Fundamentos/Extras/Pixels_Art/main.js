332201e456aec16c963c68c663133524ec6d44ad
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 19:11:09 -0300

    Cria section com quatro div e suas respectivas classes

--- Fundamentos/Extras/Pixels_Art/main.js
+++ Fundamentos/Extras/Pixels_Art/main.js
@@ -9,3 +9,23 @@ title.id = 'title';
 title.textContent = 'Color Palette';
 headerInBody.appendChild(title);
 
+//* Cria uma <section> no <body>, depois do header,
+//* com id 'color-palette'
+
+const section = document.createElement('section');
+section.id = 'color-palette';
+document.body.appendChild(section);
+
+//* Cria 4 <div> com class 'color', dentro da <section>
+//* e cada div com uma cor diferente
+
+const colors = ['black', 'cyan', 'khaki', 'orange'];
+
+for (const color of colors) {
+  const div = document.createElement('div');
+  div.classList.add('color');
+  div.classList.add(color);
+  document.querySelector('#color-palette').appendChild(div);
+};
+
+
