8ede0f47d59daf97daf6872850d2bced700ac879
jpcp0614 <jpcp0614@gmail.com>
2023-08-02 15:14:57 -0300

    Storage

---
+++ Fundamentos/DOM/Web_Storage/storage.js
@@ -0,0 +1,13 @@
+console.log(localStorage);
+
+// localStorage.setItem('firstName', 'Xablau');
+// localStorage.setItem('lastName', 'Lalaland');
+
+// console.log(localStorage.getItem('lastName'))
+
+// console.log(localStorage.removeItem('firstName'));
+
+console.log(sessionStorage);
+
+sessionStorage.setItem('firstName', 'Xablau');
+sessionStorage.setItem('lastName', 'Lalaland');
