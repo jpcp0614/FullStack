fbc89787efcbef13b459ac89ca581372f88293fe
valdenio458 <valdeniomartinho@gmail.com>
2023-08-03 16:29:19 -0300

    🥅 fix - lint

--- Fundamentos/Projetos/Pixels_Art/script.js
+++ Fundamentos/Projetos/Pixels_Art/script.js
@@ -1,11 +1,9 @@
-let divBoard = document.createElement('div');
+const divBoard = document.createElement('div');
 divBoard.id = 'pixel-board';
 document.querySelector('section').appendChild(divBoard);
 
-for(let i = 1; i <= 25; i++) {
-  let div = document.createElement('div');  
-  div.classList.add('pixel');  
+for (let i = 1; i <= 25; i += 1) {
+  const div = document.createElement('div');
+  div.classList.add('pixel');
   document.querySelector('#pixel-board').appendChild(div);
 }
-
-
