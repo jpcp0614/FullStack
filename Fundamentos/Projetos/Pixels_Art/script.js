0435bad4d1b843dcc15e51a2eedd90b43551d12d
jpcp0614 <jpcp0614@gmail.com>
2023-08-04 15:22:34 -0300

    :sparkles: feat - requirement 7

--- Fundamentos/Projetos/Pixels_Art/script.js
+++ Fundamentos/Projetos/Pixels_Art/script.js
@@ -8,5 +8,21 @@ for (let i = 1; i <= 25; i += 1) {
   document.querySelector('#pixel-board').appendChild(div);
 }
 
+//* Requisito 6
+
 const selectedBlack = document.querySelector('.black');
 selectedBlack.classList.add('selected');
+
+
+//* Requisito 7
+
+const getAllColor = document.querySelectorAll('.color');
+for (let i = 0; i < getAllColor.length; i++) {
+  getAllColor[i].addEventListener('click', changeSelected);
+}
+
+function changeSelected(event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
