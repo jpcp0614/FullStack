57200715440f226de24d55dbf63638ea44469d54
jpcp0614 <jpcp0614@gmail.com>
2023-08-05 15:59:13 -0300

    :sparkles: feat - requirement 10

--- Fundamentos/Projetos/Pixels_Art/script.js
+++ Fundamentos/Projetos/Pixels_Art/script.js
@@ -1,3 +1,6 @@
+const pixelBoardId = '#pixel-board';
+const pixelClass = '.pixel';
+
 const divBoard = document.createElement('div');
 divBoard.id = 'pixel-board';
 document.querySelector('section').appendChild(divBoard);
@@ -5,7 +8,7 @@ document.querySelector('section').appendChild(divBoard);
 for (let i = 1; i <= 25; i += 1) {
   const div = document.createElement('div');
   div.classList.add('pixel');
-  document.querySelector('#pixel-board').appendChild(div);
+  document.querySelector(pixelBoardId).appendChild(div);
 }
 
 //* Requisito 6
@@ -35,7 +38,7 @@ function selectedAndPrintPixel(e) {
   e.target.style.backgroundColor = getBackgroundColor;
 }
 
-document.querySelector('#pixel-board').addEventListener('click', selectedAndPrintPixel);
+document.querySelector(pixelBoardId).addEventListener('click', selectedAndPrintPixel);
 
 //* Requisito 9
 
@@ -46,7 +49,7 @@ const pixelBoard = document.querySelector('section');
 pixelBoard.parentNode.insertBefore(clearButton, pixelBoard);
 
 function clearPixels() {
-  const pixels = document.querySelectorAll('.pixel');
+  const pixels = document.querySelectorAll(pixelClass);
 
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
@@ -54,3 +57,51 @@ function clearPixels() {
 }
 
 clearButton.addEventListener('click', clearPixels);
+
+//* Requisito bônus 10
+
+const divInputButton = document.createElement('div');
+divInputButton.id = 'input-button';
+const divSection = document.querySelector('section');
+divSection.parentNode.insertBefore(divInputButton, divSection);
+
+// input
+const input = document.createElement('input');
+input.id = 'board-size';
+input.type = 'number';
+input.min = '1';
+divInputButton.appendChild(input);
+
+// button
+const btn = document.createElement('button');
+btn.id = 'generate-board';
+btn.textContent = 'Criar';
+divInputButton.appendChild(btn);
+
+//* apaga o quadro inicial
+const deleteBoard = () => {
+  const boardPixels = document.querySelectorAll(pixelClass);
+  for (let i = 0; i < boardPixels.length; i += 1) {
+    boardPixels[i].remove();
+  }
+};
+
+const makeBoard = (inputValue) => {
+  for (let i = 1; i <= inputValue ** 2; i += 1) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+    document.querySelector(pixelBoardId).appendChild(div);
+  }
+};
+
+//* cria um novo quadro
+const createNewBoard = () => {
+  //* input.value === '' é a mesma coisa que !input.value
+  if (input.value === '') alert('Board inválido!');
+  if (input.value >= 5 && input.value <= 50) {
+    deleteBoard();
+    makeBoard(input.value);
+  }
+};
+
+btn.addEventListener('click', createNewBoard);
