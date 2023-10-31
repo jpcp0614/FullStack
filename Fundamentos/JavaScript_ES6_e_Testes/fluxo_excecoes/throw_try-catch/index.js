d5df9cd8446595e1c7278a16508ef08d00498b67
jpcp0614 <jpcp0614@gmail.com>
2023-10-31 13:06:25 -0300

    try catch

--- Fundamentos/JavaScript_ES6_e_Testes/fluxo_excecoes/throw_try-catch/index.js
+++ Fundamentos/JavaScript_ES6_e_Testes/fluxo_excecoes/throw_try-catch/index.js
@@ -11,7 +11,7 @@ const sumB = (value1, value2) => {
 	if (typeof value1 !== 'number' || typeof value2 !== 'number') {
 		return 'Os valores devem ser numéricos!';
 	}
-  return value1 + value2;
+	return value1 + value2;
 };
 
 //console.log('Exemplo 1: 2 + 4 =', sumB(2, 4));
@@ -20,11 +20,29 @@ const sumB = (value1, value2) => {
 // Exemplo C
 
 const sumC = (value1, value2) => {
-  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+	if (typeof value1 !== 'number' || typeof value2 !== 'number') {
 		throw new Error('Os valores devem ser numéricos!');
 	}
 	return value1 + value2;
 };
 
 //console.log('Exemplo 1: 2 + 4 =', sumC(2, 4));
-console.log('Exemplo 2: 2 + "4" =', sumC(2, '4'));
+//console.log('Exemplo 2: 2 + "4" =', sumC(2, '4'));
+
+// Exemplo D
+const verifyNumber = (value1, value2) => {
+	if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+		throw new Error('Os valores devem ser numéricos!');
+	}
+};
+
+const sumD = (value1, value2) => {
+	try {
+		verifyNumber(value1, value2);
+		return value1 + value2;
+	} catch (error) {
+		return error.message;
+	}
+};
+
+console.log('Exemplo 2: 2 + "4" =', sumD(2, '4'));
