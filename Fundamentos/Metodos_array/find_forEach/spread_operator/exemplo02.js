dcacf0f768efef2d8450ba59ac2348140b70aff9
jpcp0614 <jpcp0614@gmail.com>
2023-11-29 20:10:00 -0300

    exemplo02

---
+++ Fundamentos/Metodos_array/find_forEach/spread_operator/exemplo02.js
@@ -0,0 +1,7 @@
+//* Concatenar dois arrays
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const combinedArray = [...array1, ...array2];
+
+console.log(combinedArray); // [1, 2, 3, 4, 5, 6]
