a2bd1a947a2f0e9f4c6b6baa52df83d181d0cf20
jpcp0614 <jpcp0614@gmail.com>
2023-11-29 20:15:31 -0300

    exemplo04

---
+++ Fundamentos/Metodos_array/find_forEach/spread_operator/exemplo04.js
@@ -0,0 +1,8 @@
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+
+console.log(months);
