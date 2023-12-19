38ab8efa3b685bb476605073dfba148bb43fa87f
jpcp0614 <jpcp0614@gmail.com>
2023-12-19 17:09:32 -0300

    :sparkles: feat - Requirement 06

--- Fundamentos/Projetos/Zoo_Functions/src/calculateEntry.js
+++ Fundamentos/Projetos/Zoo_Functions/src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((item) => item.age < 18).length;
+  const adult = entrants.filter((item) => item.age >= 18 && item.age < 50).length;
+  const senior = entrants.filter((item) => item.age >= 50).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+
+  const age = countEntrants(entrants);
+  const children = age.child * data.prices.child;
+  const adults = age.adult * data.prices.adult;
+  const seniors = age.senior * data.prices.senior;
+  const totalPrice = children + adults + seniors;
+
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
