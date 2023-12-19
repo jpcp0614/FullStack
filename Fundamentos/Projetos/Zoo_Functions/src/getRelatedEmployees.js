62c6f048c6997c80f5310fcfc8ef744e21ddec57
jpcp0614 <jpcp0614@gmail.com>
2023-12-19 14:05:34 -0300

    :sparkles: feat - Requirement 04

--- Fundamentos/Projetos/Zoo_Functions/src/getRelatedEmployees.js
+++ Fundamentos/Projetos/Zoo_Functions/src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((manager) => manager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((employee) => employee.managers
+      .includes(managerId))
+      .map((manager) => `${manager.firstName} ${manager.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
