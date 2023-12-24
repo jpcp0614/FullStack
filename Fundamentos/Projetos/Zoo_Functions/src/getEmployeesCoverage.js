83c3004612633dc6353dee6afdfaf36074dde9d5
jpcp0614 <jpcp0614@gmail.com>
2023-12-23 21:40:44 -0300

    :sparkles: feat - Requirement 10 - part 01

--- Fundamentos/Projetos/Zoo_Functions/src/getEmployeesCoverage.js
+++ Fundamentos/Projetos/Zoo_Functions/src/getEmployeesCoverage.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const employee = (obj) =>
+  employees.find((item) => item.firstName === obj.name || item.lastName === obj.name);
+
+const funcIds = (obj) => employee(obj).responsibleFor;
+
+const funcSpecies = (obj) => {
+  const ids = funcIds(obj);
+  return ids.map((elem) => species.find((item) => item.id === elem).name);
+};
+
+const funcLocations = (obj) => {
+  const ids = funcIds(obj);
+  return ids.map((elem) => species.find((item) => item.id === elem).location);
+};
+
+function getEmployeesCoverage(obj) {
+  const emp = employee(obj);
+  const specs = funcSpecies(obj);
+  const locations = funcLocations(obj);
+
+  const fullName = `${emp.firstName} ${emp.lastName}`;
+
+  return { id: emp.id, fullName, species: specs, locations };
 }
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
