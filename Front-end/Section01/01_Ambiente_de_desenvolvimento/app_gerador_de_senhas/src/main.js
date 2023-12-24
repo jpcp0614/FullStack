716701f2fac62436b8a709319bb87c100923853d
jpcp0614 <jpcp0614@gmail.com>
2023-12-24 15:17:00 -0300

    Exercício de fixação

--- Front-end/Section01/01_Ambiente_de_desenvolvimento/app_gerador_de_senhas/src/main.js
+++ Front-end/Section01/01_Ambiente_de_desenvolvimento/app_gerador_de_senhas/src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
@@ -7,4 +8,9 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', () => {
+  copy(displayPasswordEl.textContent);
+  alert('Senha copiada!');
 });
