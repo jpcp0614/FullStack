ad9489ca6a8d9dc5199d71a8815c92ef99ea643d
jpcp0614 <jpcp0614@gmail.com>
2023-12-24 14:43:42 -0300

    npx e npm

--- Front-end/Section01/01_Ambiente_de_desenvolvimento/app_gerador_de_senhas/src/main.js
+++ Front-end/Section01/01_Ambiente_de_desenvolvimento/app_gerador_de_senhas/src/main.js
@@ -1,3 +1,9 @@
 import { nanoid } from 'nanoid';
 
-console.log(nanoid());
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
