0f82a06d26b9b9433c5ea4695eebc89ed2c097de
jpcp0614 <jpcp0614@gmail.com>
2023-12-27 10:52:15 -0300

    Fixação 02

--- Front-end/Section01/02_JavaScript_assincrono/fixacao_microblogs/index.js
+++ Front-end/Section01/02_JavaScript_assincrono/fixacao_microblogs/index.js
@@ -9,11 +9,19 @@ import {
 } from './utils/updateUI';
 
 const USERS_API = 'https://dummyjson.com/user';
+const POSTS_SELECT_API = 'https://dummyjson.com/posts/user/';
 
 fetch(USERS_API)
   .then(response => response.json())
-  .then(data => {
-		const { users } = data;
-		
-		fillUsersSelect(users);
-	})
+  .then(({ users }) => fillUsersSelect(users));
+
+const selectUsers = document.querySelector('#users-select');
+
+selectUsers.addEventListener('change', () => {
+	clearPageData();
+	const userID = selectUsers.value;
+
+	fetch(`${POSTS_SELECT_API}${userID}`)
+		.then(response => response.json())
+		.then(({ posts }) => console.log(posts))
+});
