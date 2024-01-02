a7f7fa2a21bd9e0c8616916cf3f3e059928453ad
jpcp0614 <jpcp0614@gmail.com>
2024-01-02 19:20:28 -0300

    Funções iniciais

---
+++ Front-end/Section01/01_Ambiente_de_desenvolvimento/app_blackjack/src/helpers/fetchFunctions.js
@@ -0,0 +1,11 @@
+const startNewGame = () => console.log('startNewGame');
+
+const drawCard = () => console.log('drawCard');
+
+const stopGame = () => console.log('stopGame');
+
+export {
+  startNewGame,
+  drawCard,
+  stopGame,
+};
--- Front-end/Section01/01_Ambiente_de_desenvolvimento/app_blackjack/src/main.js
+++ Front-end/Section01/01_Ambiente_de_desenvolvimento/app_blackjack/src/main.js
@@ -1,1 +1,11 @@
 import './styles.css';
+
+import { startNewGame, drawCard, stopGame } from './helpers/fetchFunctions';
+
+const shuffleButton = document.querySelector('.shuffle');
+const stopButton = document.querySelector('.stop');
+const drawButton = document.querySelector('.draw');
+
+shuffleButton.addEventListener('click', startNewGame);
+stopButton.addEventListener('click', stopGame);
+drawButton.addEventListener('click', drawCard);
