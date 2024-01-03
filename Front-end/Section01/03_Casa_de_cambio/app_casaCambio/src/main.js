2cde9d353df17d0631cfcfbdcd0ac62ba0c6412d
jpcp0614 <jpcp0614@gmail.com>
2024-01-03 20:51:00 -0300

    Tratamento de erro

--- Front-end/Section01/03_Casa_de_cambio/app_casaCambio/src/main.js
+++ Front-end/Section01/03_Casa_de_cambio/app_casaCambio/src/main.js
@@ -3,6 +3,8 @@ import './style.css';
 import Swal from 'sweetalert2';
 
 import fetchApiExchangeRate from './helpers/fetchApi';
+import { funcSubtitle, funcCoinsElements } from './helpers/createElements';
+import { inputError, coinError } from './helpers/error';
 
 const myKey = import.meta.env.VITE_API_KEY;
 const API_URL = `https://v6.exchangerate-api.com/v6/${myKey}/latest`;
@@ -13,45 +15,18 @@ const frame = document.querySelector('.frame');
 
 btn.addEventListener('click', () => {
   const coin = inputCoin.value.toUpperCase();
+  inputError(coin);
 
   fetchApiExchangeRate(`${API_URL}/${coin}`)
 		.then((data) => {
-      const subtitle = document.createElement('h2');
-      subtitle.innerHTML = `Valores referentes a 1 ${coin}`;
-      subtitle.className = 'subtitle';
-      frame.innerHTML = '';
-
-      const coinsElements = Object.keys(data.conversion_rates).map((currency) => {
-        const icon = document.createElement('img');
-        const nameOfCoin = document.createElement('h3');
-        const valueOfCoin = document.createElement('h4');
-        icon.src = './src/assets/coins.svg';
-        nameOfCoin.innerHTML = currency;
-        valueOfCoin.innerHTML = data.conversion_rates[currency].toFixed(3);
-
-        const coinInfo = document.createElement('div');
-        coinInfo.className = 'cards-coin';
-
-        const imgAndCoin = document.createElement('div');
-        imgAndCoin.className = 'img-coin';
-
-        coinInfo.appendChild(imgAndCoin);
-        imgAndCoin.appendChild(icon);
-        imgAndCoin.appendChild(nameOfCoin);
-        coinInfo.appendChild(valueOfCoin);
-
-        return coinInfo;
-      });
+      coinError(data, coin);
+      console.log(data);
+      const subtitle = funcSubtitle(coin);
+      frame.innerHTML = ''; // limpar os campos de moedas
+      
+      const coinsElements = funcCoinsElements(data);
       frame.appendChild(subtitle);
       coinsElements.forEach((coinsEl) => frame.appendChild(coinsEl));
-
     })
-		.catch((error) =>
-			Swal.fire({
-				title: 'Error!',
-				text: error.message,
-				icon: 'error',
-				confirmButtonText: 'Cool',
-			})
-		);
+    .catch((error) => console.log(error.message));
 });
