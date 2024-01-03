bc940594e33b33818636887a638aa82d12cb20c9
jpcp0614 <jpcp0614@gmail.com>
2024-01-03 13:26:57 -0300

    Funções no main com sucesso

--- Front-end/Section01/02_JavaScript_assincrono/exercicios/app_superHero/src/main.js
+++ Front-end/Section01/02_JavaScript_assincrono/exercicios/app_superHero/src/main.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+import getApiHeroes from './helpers/fetchApi';
+import randomNumber from './helpers/randomNumber';
+
+const button = document.querySelector('#button');
+const name = document.querySelector('#name');
+const image = document.querySelector('#image');
+
+button.addEventListener('click', () => {
+	const id = randomNumber();
+	getApiHeroes(id).then((data) => {
+
+		name.innerHTML = data.name;
+		image.src = data.images.md;
+	});
+});
