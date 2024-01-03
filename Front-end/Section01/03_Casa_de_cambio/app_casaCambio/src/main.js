b675486ffc5d5d8c9905c4c4b1a0a85b0508d846
jpcp0614 <jpcp0614@gmail.com>
2024-01-03 20:03:31 -0300

    Valores das moedas

--- Front-end/Section01/03_Casa_de_cambio/app_casaCambio/src/main.js
+++ Front-end/Section01/03_Casa_de_cambio/app_casaCambio/src/main.js
@@ -8,10 +8,44 @@ const myKey = import.meta.env.VITE_API_KEY;
 const API_URL = `https://v6.exchangerate-api.com/v6/${myKey}/latest`;
 
 const btn = document.querySelector('#pesquisar');
+const inputCoin = document.querySelector('#input-coin');
+const frame = document.querySelector('.frame');
 
 btn.addEventListener('click', () => {
-  fetchApiExchangeRate(`${API_URL}/USD`)
-		.then((data) => console.log(data))
+  const coin = inputCoin.value.toUpperCase();
+
+  fetchApiExchangeRate(`${API_URL}/${coin}`)
+		.then((data) => {
+      const subtitle = document.createElement('h2');
+      subtitle.innerHTML = `Valores referentes a 1 ${coin}`;
+      subtitle.className = 'subtitle';
+      frame.innerHTML = '';
+
+      const coinsElements = Object.keys(data.conversion_rates).map((currency) => {
+        const icon = document.createElement('img');
+        const nameOfCoin = document.createElement('h3');
+        const valueOfCoin = document.createElement('h4');
+        icon.src = './src/assets/coins.svg';
+        nameOfCoin.innerHTML = currency;
+        valueOfCoin.innerHTML = data.conversion_rates[currency].toFixed(3);
+
+        const coinInfo = document.createElement('div');
+        coinInfo.className = 'cards-coin';
+
+        const imgAndCoin = document.createElement('div');
+        imgAndCoin.className = 'img-coin';
+
+        coinInfo.appendChild(imgAndCoin);
+        imgAndCoin.appendChild(icon);
+        imgAndCoin.appendChild(nameOfCoin);
+        coinInfo.appendChild(valueOfCoin);
+
+        return coinInfo;
+      });
+      frame.appendChild(subtitle);
+      coinsElements.forEach((coinsEl) => frame.appendChild(coinsEl));
+
+    })
 		.catch((error) =>
 			Swal.fire({
 				title: 'Error!',
