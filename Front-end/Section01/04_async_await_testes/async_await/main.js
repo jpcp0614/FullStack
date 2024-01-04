1789bf8f486522073aea9d947732a73c5a5e5207
jpcp0614 <jpcp0614@gmail.com>
2024-01-04 15:16:48 -0300

    Usando async await com try catch

--- Front-end/Section01/04_async_await_testes/async_await/main.js
+++ Front-end/Section01/04_async_await_testes/async_await/main.js
@@ -6,20 +6,39 @@ const author = document.querySelector('.author');
 const error = document.querySelector('.error');
 
 //* THEN CATCH
-const fetchApi = () => {
-	return fetch(API_URL).then((response) => response.json());
+// const fetchApi = () => {
+// 	return fetch(API_URL).then((response) => response.json());
+// };
+
+// getQuoteBtn.addEventListener('click', () => {
+// 	fetchApi()
+//     .then((data) => {
+//       quote.innerText = data.quote;
+//       author.innerText = data.author;
+//       error.innerText = '';
+// 	  })
+//     .catch((err) => {
+//       quote.innerText = '';
+// 			author.innerText = '';
+// 			error.innerText = `Error getting quote: ${err.message}`;
+//     });
+// });
+
+const fetchApi = async () => {
+	const response = await fetch(API_URL);
+	const data = await response.json();
+	return data;
 };
 
-getQuoteBtn.addEventListener('click', () => {
-	fetchApi()
-    .then((data) => {
-      quote.innerText = data.quote;
-      author.innerText = data.author;
-      error.innerText = '';
-	  })
-    .catch((err) => {
-      quote.innerText = '';
-			author.innerText = '';
-			error.innerText = `Error getting quote: ${err.message}`;
-    });
+getQuoteBtn.addEventListener('click', async () => {
+	try {
+		const data = await fetchApi();
+		quote.innerText = data.quote;
+		author.innerText = data.author;
+		error.innerText = '';
+	} catch (err) {
+		quote.innerText = '';
+		author.innerText = '';
+		error.innerText = `Error getting quote: ${err.message}`;
+	}
 });
