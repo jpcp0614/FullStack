21b37aac89ca4638fa586edd078f5c2cc2365edb
jpcp0614 <jpcp0614@gmail.com>
2024-01-04 16:21:22 -0300

    Usando fetch com async await

--- Front-end/Section01/04_async_await_testes/app_consulta_cep/src/main.js
+++ Front-end/Section01/04_async_await_testes/app_consulta_cep/src/main.js
@@ -1,1 +1,24 @@
 import './style.css';
+
+const inputEl = document.querySelector('.input-cep');
+const buttonEl = document.querySelector('.button-cep');
+const preEl = document.querySelector('.address');
+
+const API_URL = 'https://viacep.com.br/ws/';
+
+const fetchApi = async (cep) => {
+	const response = await fetch(`${API_URL}/${cep}/json/`);
+	const data = await response.json();
+	return data;
+};
+
+buttonEl.addEventListener('click', async () => {
+	const cep = inputEl.value;
+
+	try {
+		const data =  await fetchApi(cep);
+		preEl.innerHTML = JSON.stringify(data);
+	} catch (error) {
+		return console.log(error.message);
+	}
+});
