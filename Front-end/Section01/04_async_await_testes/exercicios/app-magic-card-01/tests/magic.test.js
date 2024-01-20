2b44b2a190bdaa9cc811cf64fc76fe249139c1ba
jpcp0614 <jpcp0614@gmail.com>
2024-01-20 16:32:10 -0300

    Exercício 01.2

--- Front-end/Section01/04_async_await_testes/exercicios/app-magic-card-01/tests/magic.test.js
+++ Front-end/Section01/04_async_await_testes/exercicios/app-magic-card-01/tests/magic.test.js
@@ -1,10 +1,12 @@
 import '../simulator/fetchSimulator.js';
 import { getMagicCard } from '../src/magic.js';
-import { card } from '../simulator/card.js';
+// import { card } from '../simulator/card.js';
 
 describe('Testa a função getMagicCard', () => {
 	it('Deve possuir a propriedade name com o valor Ancestor"s Chosen', async () => {
 		const response = await getMagicCard('130550');
-		// implemente seus testes aqui
+		const { name } = response;
+		
+		expect(name).toBe('Ancestor\'s Chosen');
 	});
 });
